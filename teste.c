66052053baf97f24ba1c3a3c8305b0401b844904
Carla Mariana <carlamab2808@outlook.com>
2024-03-04 11:43:01 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+int somar(int n1, int n2) {
+    int soma = n1 + n2;
+    return soma;
+}
+
 int main() {
-    printf("Ola mundo");
+
+    int resultado = somar(10, 20);
+
+    printf("%d \n", resultado);
     return 0;
 }
