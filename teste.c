f44404e6d62f205aa7d26253b1b8f871b2fdc41d
Carla Mariana <carlamab2808@outlook.com>
2024-03-06 11:07:24 -0300

    .

--- teste.c
+++ teste.c
@@ -9,8 +9,15 @@ int main() {
 
 	// operador interessante 
 	printf("%d \n", sizeof(int));
+	
+	// obter dados
+	int n1, n2, resultado;
+	
+	printf("Digite dois numeros: \n");
+	scanf("%d %d", &n1, &n2);
+	resultado = somar(n1, n2);
 
-    int resultado = somar(10, 20);
+    //int resultado = somar(10, 20);
 
     printf("%d \n", resultado);
     return 0;
