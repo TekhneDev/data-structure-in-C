140f1188badc149010820ad8b72044f73d949f68
Carla Mariana <carlamab2808@outlook.com>
2024-03-08 11:10:22 -0300

    .

--- pointers.cpp
+++ pointers.cpp
@@ -7,5 +7,22 @@ int main() {
 	// Variaveis do tipo ponteiro 
 	// = Armazenar endereços de memória
 	
+	int n = 10;
+	int *p;
+	
+	
+	// Nesse exemplo está trabalhando com o endereço
+	p = &n;
+	
+	// Nesse exemplo está trabalhando com conteudo
+	*p = 20;
+	
+	printf("%d \n", n);
+	
+	// scanf - guardar o inteiro no endereço de Var 
+	// & - "Endereço de" 
+	// scanf("%d", &var);
+	
+	
 	return 0;
 }
